e5e5a3c7b11d68f887098e15dd67ada471be28a3
SayedJamil <sayedjamil16@gmail.com>
2023-07-05 15:36:45 +0530

    Changed table pagination

--- src/components/JSON.tsx
+++ src/components/JSON.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
-import { Button, Table, Space  } from "antd";
-import type { ColumnsType } from 'antd/es/table';
-import "../styles/JSON.css"; 
+import { Button, Table, Space } from "antd";
+import type { ColumnsType } from "antd/es/table";
+import "../styles/JSON.css";
 
 type Json = {
   userId: number;
@@ -27,13 +27,11 @@ const JSONPractice = () => {
         setJsons(data);
       });
   };
-  
 
-
-  const deleteJson = async(id: number) => {
+  const deleteJson = async (id: number) => {
     await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10 ${id}`, {
-        method: 'DELETE',
-      });
+      method: "DELETE",
+    });
     const updatedJsons = jsons.filter((json) => json.id !== id);
 
     setJsons(updatedJsons);
@@ -41,38 +39,46 @@ const JSONPractice = () => {
 
   const columns: ColumnsType<Json> = [
     {
-      title: 'ID',
-      dataIndex: 'id',
-      key: 'id',
-      
+      title: "ID",
+      dataIndex: "id",
+      key: "id",
     },
     {
-      title: 'Title',
-      dataIndex: 'title',
-      key: 'title',
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
     },
     {
-      title: 'Body',
-      dataIndex: 'body',
-      key: 'body',
+      title: "Body",
+      dataIndex: "body",
+      key: "body",
     },
-    
+
     {
-      title: 'Action',
-      key: 'action',
+      title: "Action",
+      key: "action",
       render: (_, record) => (
         <Space size="middle">
-          <Button type="primary" className="delete-btn" onClick={() => deleteJson(record.id)}>Delete</Button>
+          <Button
+            type="primary"
+            className="delete-btn"
+            onClick={() => deleteJson(record.id)}
+          >
+            Delete
+          </Button>
         </Space>
       ),
     },
   ];
 
-  
-  
   return (
-    <div>
-      <Table className="table" columns={columns} dataSource={jsons} />
+    <div style={{ height: "100%" }}>
+      <Table
+        className="table"
+        columns={columns}
+        dataSource={jsons}
+        pagination={{ pageSize: 5 }}
+      />
     </div>
   );
 };
